8f18c677a9e7fddd04e674874d56d545a0ce2509
habeebadnan10 <habeebadnan33@gmail.com>
2019-03-05 09:45:38 +0530

    changed fetch url

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent= ''
 
 
-    fetch(`http://localhost:3000/weather?address=${value}`).then((response) => {
+    fetch(`/weather?address=${value}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 console.log(data.error);
