500d3646e38279a461135f2a825b3693c69db350
habeebadnan10 <habeebadnan33@gmail.com>
2019-03-05 09:34:48 +0530

    add weather ui

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require ('express');
 const hbs = require('hbs');
 
+const geocode= require('./utils/geocode.js');
+const forecast = require('./utils/forecast.js')
 
 let app = express();
 let port = process.env.PORT || 3000
@@ -28,16 +30,17 @@ app.get('', (req,res)=> {
     })
 })
 
-app.get('/help' , (req , res ) => {
-    res.render('help',{
-        'title' : 'HELP',
-        'name' : 'Habeeb Adnan'
-    })
-})
 
 app.get('/about', (req,res) => {
     res.render('about', {
         'title' : 'About Me',
+        'name' : 'Habeeb Adnan'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        'title' : 'Help',
         'name' : 'Habeeb Adnan'
     })
 })
@@ -48,20 +51,66 @@ app.get('/bad', (req , res ) => {
     })
 })
 
+app.get('/weather' , (req , res ) => {
+    if(!req.query.address){
+          return res.send({
+               error : 'Please provide a address'
+           })
+    }
+    console.log(req.query);
+   let address = req.query.address;
+    geocode(address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                location,
+                'forecast' : forecastData,
+                address
+            })
+            console.log(location)
+            console.log(forecastData)
+        })
+    })
+})
+
+app.get('/products' , (req , res ) => {
+    if(!req.query.search){
+       return res.send({
+            error : 'no poduct searched'
+        })
+    }
+    console.log(req.query);
+    res.send({
+        'title' : 'bat',
+        'name' : 'kookabura'
+    })
+})
+
+
 app.get('/help/*', (req,res) => {
     res.render('404',{
-     'title' : '404',
-     'name' : 'Habeeb Adnan',
-      'text' : 'help article not found'  
+        'title' : '404',
+        'name' : 'Habeeb Adnan',
+        'text' : 'help article not found'  
     }) 
- })
+})
 
 app.get('*', (req,res) => {
-   res.render('404',{
-    'title' : '404',
-    'name' : 'Habeeb Adnan',
-     'text' : 'Page Not Found'  
-   }) 
+    res.render('404',{
+        'title' : '404',
+        'name' : 'Habeeb Adnan',
+        'text' : 'Page Not Found'  
+    }) 
 })
 
 app.listen(port,()=>{
